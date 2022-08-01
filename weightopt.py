d7ce4e1ce9c9c813453e16d1637d3979dfd1e620
Unununium4 <andrew.n.morrow@gmail.com>
2022-08-01 14:50:20 -0500

    starting on using rapid arc plans for optimization

--- weightopt.py
+++ weightopt.py
@@ -14,9 +14,18 @@ import translator as tr
 import time
 
 #cost function
-def cost(ideal, current):
-    c = 100*np.sum(abs(ideal-current)/np.max(ideal))/(len(ideal)*len(ideal[0])*len(ideal[0][0]))
-    return c #average % difference per pixel, relative to the maximum dose in the ideal set
+def cost(goalptv,goaloar, optptv,optoar): #playing with the cost function.  i think we'll need to just send it points, not the entire 3D sets to speed things up
+    maxd = np.max(goalptv)
+    n=len(goalptv)+len(goaloar)
+    c=0
+    for pt in range(len(goalptv)):
+        if optptv[pt]< goalptv[pt] or optptv[pt] > maxd:
+            c+= abs(optptv[pt]- goalptv[pt])/goalptv[pt]
+    for pt in range(len(goaloar)):
+        if optoar[pt]>goaloar[pt]:
+            c+= (optoar[pt]- goaloar[pt])/goaloar[pt]
+    c=100*c/n            
+    return c 
 
 def sumall(fielddoses, wts):
     s=fielddoses[0]*0
@@ -38,7 +47,9 @@ tempTime = time.time()
 
 exportloc = r"K:\Physics Division\Personal folders\ANM\MLMLC\data\9fldhnkpreweight"
 origfile = r"K:\Physics Division\Personal folders\ANM\MLMLC\data\9fldhnkideal\idealdose.dcm"
+finalfolder = r"K:\Physics Division\Personal folders\ANM\MLMLC\data"
 ptvnames=["PTV_total"]#as for the ptvs, we will try to keep the doses the same between the optimized plan and the ideal.  everywhere else we will minimize.
+oarnames=["Parotid_L_P", "Parotid_R_P", "Larynx_P", "Mandible_P","SpinalCanal_P"]
 origrd= pydicom.dcmread(origfile)
 
 for f in listdir(exportloc):
@@ -49,93 +60,162 @@ origdose = origrd.pixel_array * origrd.DoseGridScaling
 origin=origrd.ImagePositionPatient
 pixelspacing=origrd.PixelSpacing[0]#pixel spacing in the axial plane better be the same x vs y
 spacing = origrd.GridFrameOffsetVector[1]-origrd.GridFrameOffsetVector[0]#this is the slice spacing
-inside=[]
-
-rois=[]
-for ptv in ptvnames:
-    for r in rs.StructureSetROISequence:
-        if r.ROIName==ptv:
-            rois.append(r.ROINumber)
-
-for r in rois:
-    for s in rs.ROIContourSequence:
-        if s.ReferencedROINumber==r:
-            thiscontour = s.ContourSequence
-            break
-    for k in range(len(thiscontour)):
-        c=thiscontour[k].ContourData
-        xs = c[::3]
-        ys = c[1::3]
-        z= np.int32(c[2])#z isnt gonna change per sequence
-        minx= np.int32(np.min(xs))
-        maxx = np.int32(np.max(xs))
-        miny= np.int32(np.min(ys))
-        maxy=np.int32(np.max(ys))
-        for i in range(minx,maxx):
-            for j in range(miny,maxy):
-                if IsPointInPolygon(xs,ys,i,j):
-                    inside.append([np.round((i-origin[0])/pixelspacing),np.round((j-origin[1])/pixelspacing),np.round((z-origin[2])/spacing)])
-inside=np.array(inside, dtype=np.int32)
-
-origdose = origrd.pixel_array * origrd.DoseGridScaling
-ptvmask =0*deepcopy(origdose)
-for p in inside:
-    ptvmask[ p[2] ,p[1],p[0]]=1#whatever this is how the indices are
-
+ptvpts=[]
+oarpts=[]
 #first let's get the bounds of the RD array that we want to optimize with.  No need to optimize everywhere
 maxorigdose = np.max(origdose)
-whererelevant = np.argwhere(origdose>0.5*maxorigdose)#im just lookin at doses within 50% of the max
+whererelevant = np.argwhere(origdose>0.25*maxorigdose)#im just lookin at doses within 25% of the max
 imin = np.min(whererelevant[:,0])
 imax = np.max(whererelevant[:,0])
 jmin = np.min(whererelevant[:,1])
 jmax = np.max(whererelevant[:,1])
 kmin = np.min(whererelevant[:,2])
 kmax = np.max(whererelevant[:,2])
+
+ptvs=[]
+oars=[]
+for ptv in ptvnames:
+    for r in rs.StructureSetROISequence:
+        if r.ROIName==ptv:
+            ptvs.append(r.ROINumber)
+for oar in oarnames:
+    for o in rs.StructureSetROISequence:
+        if o.ROIName==oar:
+            oars.append(o.ROINumber)
+
+for r in ptvs:
+    for s in rs.ROIContourSequence:
+        if s.ReferencedROINumber==r:
+            thiscontour = s.ContourSequence
+            for k in range(len(thiscontour)):
+                c=thiscontour[k].ContourData
+                z= c[2]#z isnt gonna change per sequence
+                z=np.round((z-origin[2])/spacing)
+                if z<imin or z>=imax:
+                    continue
+                xs = np.multiply(np.add(c[::3],-origin[0]),1/pixelspacing)
+                ys = np.multiply(np.add(c[1::3],-origin[1]),1/pixelspacing)
+            
+                minx= max([np.int32(np.min(xs)), kmin])
+                maxx = min([np.int32(np.max(xs)), kmax])
+                miny= max([np.int32(np.min(ys)),jmin])
+                maxy=min([np.int32(np.max(ys)),jmax])
+
+                for i in range(minx,maxx):
+                    for j in range(miny,maxy):
+                        if IsPointInPolygon(xs,ys,i,j):
+                            #0,1,2-> 2,1,0
+                            ptvpts.append([z,j,i])
+for o in oars:
+    for s in rs.ROIContourSequence:
+        if s.ReferencedROINumber==o:
+            thiscontour = s.ContourSequence
+            for k in range(len(thiscontour)):
+                c=thiscontour[k].ContourData
+                z= c[2]#z isnt gonna change per sequence
+                z=np.round((z-origin[2])/spacing)
+                if z<imin or z>=imax:
+                    continue
+                xs = np.multiply(np.add(c[::3],-origin[0]),1/pixelspacing)
+                ys = np.multiply(np.add(c[1::3],-origin[1]),1/pixelspacing)
+            
+                minx= max([np.int32(np.min(xs)), kmin])
+                maxx = min([np.int32(np.max(xs)), kmax])
+                miny= max([np.int32(np.min(ys)),jmin])
+                maxy=min([np.int32(np.max(ys)),jmax])
+
+                for i in range(minx,maxx):
+                    for j in range(miny,maxy):
+                        if IsPointInPolygon(xs,ys,i,j):
+                            #coordinate axes are 0,1,2-> 2,1,0
+                            oarpts.append([z,j,i])
+
+oarpts=np.array(oarpts, dtype=np.int32)
+ptvpts=np.array(ptvpts, dtype=np.int32)
+imin=min([min(np.transpose(ptvpts)[0]), min(np.transpose(oarpts)[0])])
+jmin=min([min(np.transpose(ptvpts)[1]), min(np.transpose(oarpts)[1])])
+kmin=min([min(np.transpose(ptvpts)[2]), min(np.transpose(oarpts)[2])])
+imax=max([max(np.transpose(ptvpts)[0]), max(np.transpose(oarpts)[0])])+1
+jmax=max([max(np.transpose(ptvpts)[1]), max(np.transpose(oarpts)[1])])+1
+kmax=max([max(np.transpose(ptvpts)[2]), max(np.transpose(oarpts)[2])])+1
+for p in oarpts:
+    p[0]+=-imin
+    p[1]+=-jmin
+    p[2]+=-kmin
+for p in ptvpts:
+    p[0]+=-imin
+    p[1]+=-jmin
+    p[2]+=-kmin
+
 goaldose = origdose[imin:imax,jmin:jmax,kmin:kmax]
-mask = ptvmask[imin:imax,jmin:jmax,kmin:kmax]#not using this just yet
+goalptvcloud=[]
+for pt in ptvpts:
+    goalptvcloud.append(goaldose[pt[0],pt[1],pt[2]])
+goaloarcloud=[]
+for pt in oarpts:
+    goaloarcloud.append(goaldose[pt[0],pt[1],pt[2]])
+
+goalptvcloud = np.asarray(goalptvcloud)
+goaloarcloud = np.asarray(goaloarcloud)
 
 #get the mlmlc field doses
-rds =[]
+#rds =[]
+preptvcloud=[]
+preoarcloud=[]
 count=0
 for f in listdir(exportloc):
     if isfile(join(exportloc, f)):
         if f[0:2]=="RD":
             tempd = pydicom.dcmread(exportloc +"\\"+f)
             scale = tempd.DoseGridScaling
-            temppa = tempd.pixel_array * scale
-            rds.append(temppa[imin:imax,jmin:jmax,kmin:kmax])
+            temppa = (tempd.pixel_array * scale)[imin:imax,jmin:jmax,kmin:kmax]
+            tempptvcloud=[]
+            tempoarcloud=[]
+            for pt in ptvpts:
+                tempptvcloud.append(temppa[pt[0],pt[1],pt[2]])
+            for pt in oarpts:
+                tempoarcloud.append(temppa[pt[0],pt[1],pt[2]])
+            preptvcloud.append(tempptvcloud)
+            preoarcloud.append(tempoarcloud)
         if f[0:2]=="RP":
             rp = pydicom.dcmread(exportloc +"\\"+f)
         print("\r loaded file "+ str(count+1) + " of " + str(len(listdir(exportloc))))
         count+=1
 #now do the optimization
-weights = np.ones(len(rds))
+preptvcloud = np.asarray(preptvcloud)
+preoarcloud = np.asarray(preoarcloud)
+weights = np.ones(len(preptvcloud))
 notopt = True
 counter = 1
 wtd=[0.01,-0.01]#we're gonna search by steps of 1%
 
-allsum = sumall(rds, weights)
-
 #need to first normalize to the mean dose between the mean dose to the sets so that we dont have to search quite as hard.
-normalize = np.mean(goaldose)/np.mean(allsum)
+normalize = np.mean(goalptvcloud)/np.mean(sumall(preptvcloud, weights))
 
 print("normalized by " + str(normalize))
 
 weights *= normalize
-allsum = sumall(rds, weights)
-oldcost = cost(goaldose,allsum)
+allsumptv = sumall(preptvcloud, weights)
+alloarptv = sumall(preoarcloud, weights)
+oldcost = cost(goalptvcloud,goaloarcloud, allsumptv,alloarptv)
 newcost=oldcost
+iterchangearray=[]
 while notopt:
     notopt=False#if this doesnt switch to True at the deepest part of these loops, exit
     wtdn = deepcopy(wtd)
-    for b in range(len(rds)):
+    itercost= newcost
+    for b in range(len(preptvcloud)):
         wtdn = deepcopy(wtd)
-        otherbeams = deepcopy(rds)
-        del otherbeams[b]
+        otherbeamsptv = deepcopy(preptvcloud)
+        otherbeamsoar = deepcopy(preoarcloud)
+
+        otherbeamsptv=np.delete(otherbeamsptv,b,0)
+        otherbeamsoar=np.delete(otherbeamsoar,b,0)
+
         otherweights = deepcopy(weights)
         otherweights= np.delete(otherweights, b)
-        othersum = sumall(otherbeams, otherweights) #reduce the number of calculations we have to do when optimizing weight per beam
-
+        othersumptv = sumall(otherbeamsptv, otherweights) #reduce the number of calculations we have to do when optimizing weight per beam
+        othersumoar = sumall(otherbeamsoar, otherweights) 
         while len(wtdn)>0:
             oldcost= newcost
             #weights[b]+=wtdn[0]
@@ -146,9 +226,12 @@ while notopt:
                 # if len(wtdn)==1:
                 #     del wtdn[0]
             else: weights[b]+=wtdn[0]
-            allsum= rds[b]*weights[b] + othersum
-            newcost = cost(goaldose,allsum)
-            if newcost>oldcost :
+
+            allsumptv= preptvcloud[b]*weights[b] + othersumptv
+            allsumoar= preoarcloud[b]*weights[b] + othersumoar
+
+            newcost = cost(goalptvcloud,goaloarcloud, allsumptv,allsumoar)
+            if newcost>=oldcost :
                 if weights[b]==0: weights[b]=oldw
                     
                 else: weights[b] -= wtdn[0]
@@ -161,8 +244,16 @@ while notopt:
                 if weights[b]==0:
                     del wtdn[0]
     counter+=1
+    iterchange = 100*(itercost-newcost)/newcost
+    print("Change over this iteration: " + str(iterchange)[:4]+"%")
+    iterchangearray.append(iterchange)
+    if iterchange <0.1:
+        notopt = False#if only 0.1%change in cost from the new iteration go ahead and complete
+        print("Optimization completed due to minimal change in cost between iterations.")    
     if counter==100:
-        notopt=False#stop after n iterations        
-tr.rewriteWeights(origfile, exportloc,weights, "HNMLCfinal")
+        notopt=False#stop after n iterations    
+        print("optimization completed due to meeting the iterations limit")    
+tr.rewriteWeights(origfile, exportloc,weights, finalfolder + r"\HNMLCfinal")
 t = (time.time() - tempTime)
+np.savetxt(finalfolder+r"\HNMLCcostf.csv",iterchangearray,delimiter =',')
 print("Weight optimization completed in " + str(t)[:6]+" seconds")
